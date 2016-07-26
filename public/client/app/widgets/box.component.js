890d0c0dc1d56c57e048b61221cd8f0819de80c5
Thiago Antonius Oliveira Souza <thiagoantonius.souza@prodeb.ba.gov.br>
2016-07-26 17:11:15 -0300

    Mudança de diretiva para component

---
+++ public/client/app/widgets/box.component.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+   /** @ngInject */
+  angular
+    .module('app')
+    .component('box', {
+      templateUrl: function(Global) {
+        return Global.clientPath + '/widgets/box.html'
+      },
+      replace: true,
+      transclude: true,
+      bindings: {
+        title: '@',
+        width: '@'
+      }
+    });
+}());
