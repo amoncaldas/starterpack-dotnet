4e7dedcf8cdcb9e101db5c58b1ba62129be59195
Thiago Antonius Oliveira Souza <thiagoantonius.souza@prodeb.ba.gov.br>
2016-08-02 14:40:22 -0300

    Ajustado a tela de usuário e perfil

--- public/client/app/app.config.js
+++ public/client/app/app.config.js
@@ -11,7 +11,9 @@
     // route the JWT should be retrieved from
     $authProvider.loginUrl = Global.apiVersion + '/authenticate';
     $mdThemingProvider.theme('default')
-      .primaryPalette('blue')
+      .primaryPalette('blue', {
+        default: '700'
+      })
       .accentPalette('red')
       .warnPalette('deep-orange');
 
--- public/client/app/widgets/box.component.js
+++ public/client/app/widgets/box.component.js
@@ -5,16 +5,16 @@
   angular
     .module('app')
     .component('box', {
+      replace: true,
       templateUrl: function(Global) {
         return Global.clientPath + '/widgets/box.html'
       },
-      replace: true,
       transclude: {
         toolbarButtons: '?boxToolbarButtons',
         footerButtons: '?boxFooterButtons'
       },
       bindings: {
-        title: '@'
+        boxTitle: '@'
       },
       controller: function($transclude) {
         var ctrl = this;
