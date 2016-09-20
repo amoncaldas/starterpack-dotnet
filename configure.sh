9a931eb82c788cb237df9f113ff21e1658545b81
Thiago Antonius <thiagoaos@gmail.com>
2016-09-20 16:17:43 -0300

    Aumentando o tempo do composer install

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@ npm install
 bower install
 chmod **777** -R storage
 chmod **777** -R bootstrap/cache
-composer install
+COMPOSER_PROCESS_TIMEOUT=2000 composer install
 php artisan key:generate
 php artisan jwt:secret
 php artisan migrate --seed
