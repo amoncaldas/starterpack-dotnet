2e5b0c53d8c15c50c47e773fbb5b1d31795caf7e
Thiago Antonius Oliveira Souza <thiagoantonius.souza@prodeb.ba.gov.br>
2017-04-05 10:18:05 -0300

    Adicionado o restante do arcabouço operações CRUD.

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -22,5 +22,45 @@ namespace starterpack.Controllers
         {
             return _context.Users.ToList();
         }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return _context.Users.FirstOrDefault(x => x.Id == id);
+        }
+
+        // POST api/users
+        [HttpPost]
+        public IActionResult Post([FromBody]User value)
+        {
+            _context.Users.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        } 
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]User value)
+        {
+            var selected = _context.Users.FirstOrDefault(x => x.Id == id);
+            if (selected != null)
+            {
+                _context.Entry(selected).Context.Update(value);
+                _context.SaveChanges();
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var selected = _context.Users.FirstOrDefault(x => x.Id == id);
+            if (selected != null)
+            {
+                _context.Users.Remove(selected);
+                _context.SaveChanges();
+            }
+        }               
     }
 }
